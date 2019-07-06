4e3e6c8470a9aee375e35f3fc2ecdba749e20d1c
Khofidin <offiedz@gmail.com>
2019-07-06 08:32:33 +0700

    init

---
+++ app/src/main/java/in/khofid/smslistenerapp/SmsReceiver.java
@@ -0,0 +1,54 @@
+package in.khofid.smslistenerapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    final String TAG = SmsReceiver.class.getSimpleName();
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final Bundle bundle = intent.getExtras();
+        try {
+            if(bundle != null) {
+                final Object[] pduObj = (Object[]) bundle.get("pdus");
+                if(pduObj != null) {
+                    for (Object aPdusObj: pduObj) {
+                        SmsMessage currentMessage = getIncomingMessage(aPdusObj, bundle);
+                        String senderNum = currentMessage.getDisplayOriginatingAddress();
+                        String message = currentMessage.getDisplayMessageBody();
+                        Log.d(TAG, "senderNum: " + senderNum + "; message: " + message);
+                        Intent showSmsIntent = new Intent(context, SmsReceiverActivity.class);
+                        showSmsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_NO, senderNum);
+                        showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_MESSAGE, message);
+                        context.startActivity(showSmsIntent);
+                    }
+                } else {
+                    Log.d(TAG, "onReceive: SMS is null");
+                }
+            }
+        } catch (Exception e){
+            Log.d(TAG, "Exception smsReceiver" + e);
+        }
+    }
+
+    private SmsMessage getIncomingMessage(Object aObject, Bundle bundle) {
+        SmsMessage currentSms;
+        if(Build.VERSION.SDK_INT >= 23) {
+            String format = bundle.getString("format");
+            currentSms = SmsMessage.createFromPdu((byte[]) aObject, format);
+        } else {
+            currentSms = SmsMessage.createFromPdu((byte[]) aObject);
+        }
+
+        return currentSms;
+    }
+}
